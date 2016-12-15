75d8a907c9b0070e8c24ab9ff35de7053c34fe7d
Kevin Sawicki <kevinsawicki@gmail.com>
2016-12-15 13:20:17 -0800

    Rename option to contextIsolation

--- spec/api-browser-window-spec.js
+++ spec/api-browser-window-spec.js
@@ -1796,7 +1796,7 @@ describe('BrowserWindow module', function () {
     })
   })
 
-  describe('isolated option', () => {
+  describe('contextIsolation option', () => {
     it('separates the page context from the Electron/preload context', (done) => {
       if (w != null) w.destroy()
 
@@ -1821,7 +1821,7 @@ describe('BrowserWindow module', function () {
       w = new BrowserWindow({
         show: false,
         webPreferences: {
-          isolated: true,
+          contextIsolation: true,
           preload: path.join(fixtures, 'api', 'isolated-preload.js')
         }
       })
